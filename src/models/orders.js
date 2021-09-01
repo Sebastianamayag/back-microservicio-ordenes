98b1ea7999172ac7819f8f8b6584b65af1aac948
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2021-09-01 07:35:23 -0500

    changes

--- src/models/orders.js
+++ src/models/orders.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 const Order=new Schema({
-    fecha:Date,
+    fecha:String,
     precio:Number,
     cantidad:Number,
     id_producto:Number,
